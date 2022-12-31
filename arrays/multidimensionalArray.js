c3445e638c16a0f81e4e807e9e92c14bfff5bb96
Deivid Almeida <deividalmeida365@gmail.com>
2022-12-31 15:46:11 -0300

    multidimensional arrays

---
+++ arrays/multidimensionalArray.js
@@ -0,0 +1,31 @@
+var dinnerChoices = [["Salad", "Soup", "Cheese Plate"], ["Chicken", "Salmon", "lasagna"]]
+
+// entrada
+let appetizerIndex = 0 
+
+// prato principal
+let mainDishIndex = 1
+
+let firstAppetizer = dinnerChoices[appetizerIndex][0]
+
+let secoundAppetizer = dinnerChoices[appetizerIndex][1]
+
+let thirdAppetizer = dinnerChoices[appetizerIndex][2]
+
+let allAppetizer = [firstAppetizer, secoundAppetizer, thirdAppetizer]
+
+console.log("Todos os pratos de Entrada: ", allAppetizer)
+
+
+let firstMainDish = dinnerChoices[mainDishIndex][0]
+
+let secoundMainDish = dinnerChoices[mainDishIndex][1]
+
+let thirdMainDish = dinnerChoices[mainDishIndex][2]
+
+let allMainDish = [firstMainDish, secoundMainDish, thirdMainDish]
+
+
+
+console.log("Todos os pratos Principais: ", allMainDish)
+
