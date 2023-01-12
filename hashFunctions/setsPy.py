00caeaf44c439869d069f52b670158cc3cefe56b
Deivid Almeida <deividalmeida365@gmail.com>
2023-01-12 13:44:14 -0300

    python dictionary

---
+++ hashFunctions/setsPy.py
@@ -0,0 +1,15 @@
+primaryColors = frozenset(["Red", "Blue", "yellow"])
+
+
+color = "green"
+
+if color.lower() in primaryColors:
+    print(color + "is a primary color")
+else: 
+    print(color + " is not a primary color")
+    
+letters = set(['a', 'b'])
+
+letters.add('c')
+
+print(letters)
