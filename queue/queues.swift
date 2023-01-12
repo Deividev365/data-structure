693535cb3817a54239a5eb29a9e6191eb8b7e048
Deivid Almeida <deividalmeida365@gmail.com>
2023-01-12 11:42:52 -0300

    queues

---
+++ queue/queues.swift
@@ -0,0 +1,50 @@
+// card deck stack algorithm
+
+
+class Queue {
+    
+    var queueArray = [String]()
+    // enqueue array
+    
+    func enqueue(item:String) {
+        self.queueArray.append(item)
+    }
+    
+    // dequeue arrau
+    
+    func dequeue() -> String? {
+       if self.queueArray.first != nil {
+            let firstString = self.queueArray.first
+            self.queueArray.removeFirst()
+            return firstString!
+        } else {
+            return nil
+        } 
+    }
+    // peek
+    
+    func peek() -> String? {
+        if self.queueArray.first != nil {
+            return self.queueArray.first
+        } else {
+            return nil
+        }
+    }
+}
+
+
+var myQueue = Queue()
+
+myQueue.enqueue(item: "Peggy" )
+myQueue.enqueue(item: "Larry")
+myQueue.enqueue(item: "Serena")
+
+
+
+print(myQueue.peek()!)
+print(myQueue.peek()!)
+
+var firstToLeave = myQueue.dequeue()
+
+print(myQueue.peek()!)
+
